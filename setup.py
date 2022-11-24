e1602151544ff7bce73f707cba14111d018105c3
HolaDude <106875707+HolaDuded@users.noreply.github.com>
2022-11-23 20:39:34 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+pixray_version = "latest release" #@param ["latest release", "next planned release"]
+branch = "release"
+if pixray_version == "next planned release":
+  branch = "master"
+!rm -Rf pixray
+!git clone --recursive --branch $branch https://github.com/pixray/pixray
+!pip install -r pixray/requirements.txt
+!pip install basicsr
+!pip uninstall -y tensorflow 
+!git clone https://github.com/pixray/diffvg
+%cd diffvg
+!git submodule update --init --recursive
+!python setup.py install
+%cd ..
+!pip freeze | grep torch
+
+import sys
+sys.path.append("pixray")
+import pixray
